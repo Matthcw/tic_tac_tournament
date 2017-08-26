ea94e5910618b9ab235a45acb245675a0f93fde5
Matthew M <matthcw@hotmail.co.uk>
2017-08-26 17:08:45 +0100

    before changing to expressjs

--- app.js
+++ app.js
@@ -2,20 +2,45 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 
+const io = require('socket.io')();
+
 server.connection({port: 3000});
 
-server.register(require('inert'), err => {
+server.register([require('inert'), require('vision')], err => {
   if(err) throw err;
+
+  server.start( () => {
+    console.log("Listening on " + server.info.uri);
+  });
+});
+
+server.views({
+  engines:{
+    html: require('handlebars')
+  },
+  path: './views'
 });
 
 server.route({
   path: '/',
   method: 'GET',
   handler: (request, reply) => {
-    reply.file('public/index.html');
+    reply.view('index', {
+      uri: server.info.uri
+    });
   }
 })
 
-server.start( () => {
-  console.log("Listening on " + server.info.uri);
+
+
+io.attach(server.listener);
+
+io.on('connection', (socket) => {
+  console.log("New user.");
+
+  socket.on("buttonClicked", (id) => {
+    console.log("yaaaH" + id);
+    io.emit("addX", id);
+  });
+
 });
